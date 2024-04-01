ac8e4bff3f7d982aeec7db3096702f0bbacf3572
Wecreds <wecredsbr@gmail.com>
2024-04-01 14:40:03 -0300

    feat: Criando tabela "acessórios" e sua API Rest.

--- garage/models.py
+++ garage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Accessory(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name = "Accessory"
+        verbose_name_plural = "Accessories"
--- garage/views.py
+++ garage/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garage.models import Accessory
+from garage.serializers import AccessorySerializer
+
+class AccessoryViewSet(ModelViewSet):
+    queryset = Accessory.objects.all()
+    serializer_class = AccessorySerializer
